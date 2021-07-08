522813e82b6e0a608b5e439b331edf2fc86f263b
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-08 22:03:14 +0200

    Final example

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,13 @@
 // An array containing HTML elements
 const numbersList = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
 
-function SimpleList() {
+function List() {
   return (
     <div className="list">
-      <h2>SimpleList</h2>
+      <h2>List</h2>
       {numbersList}
     </div>
   );
 }
 
-export default SimpleList;
+export default List;
