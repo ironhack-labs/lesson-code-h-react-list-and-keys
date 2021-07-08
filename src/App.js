01c8881a7fe40a99fcd4aeb258f2ee85afc72929
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-08 19:36:27 +0200

    Example 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
 import SimpleList from "./components/SimpleList";
-import StudentList from "./components/StudentList";
-import ImprovedStudentList from "./components/ImprovedStudentList";
-import ProjectList from "./components/ProjectList";
-import MovieList from "./components/MovieList";
-
 
 function App() {
   return (
     <div className="App">
-      {/* <SimpleList /> */}
-      {/* <StudentList /> */}
-      {/* <ImprovedStudentList /> */}
-      {/* <ProjectList /> */}
-      <MovieList />
+      <SimpleList />
     </div>
   );
 }
