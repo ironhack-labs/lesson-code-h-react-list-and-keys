8bf054125de20dcc45349becba4649d8d3f4313c
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-08 19:13:12 +0200

    Final example

--- src/components/SimpleList.js
+++ src/components/SimpleList.js
@@ -1,7 +1,4 @@
-// 1
-const numbers = [1, 2, 3, 4];
-
-// array of list item HTML elements that needs to be displayed
+// An array containing HTML elements
 const numbersList = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
 
 function SimpleList() {
