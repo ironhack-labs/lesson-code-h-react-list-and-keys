e8d9d6c552882a5cd882948ab0ff83ff71c04be0
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-09 02:13:10 +0200

    Example 7

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,9 @@
 import { useState } from 'react';
-
-// Array of movies
 import moviesData from '../movies-data.json';
 
+import MovieCard from './MovieCard';
+
 function MovieList() {
-  // Declare a state variable "movies"
-  // and set the array from movies-data.json as the initial state
   const [movies, setMovies] = useState(moviesData);
 
   const deleteMovie = (movieId) => {
@@ -20,16 +18,7 @@ function MovieList() {
     <div>
       <h2>Movie List</h2>
       {movies.map((movie) => {
-        return (
-          <div key={movie._id} className="MovieCard">
-            <h3>{movie.title}</h3>
-            <p>Director: {movie.director}</p>
-            <p>Rating: {movie.IMDBRating}</p>
-            <button onClick={() => deleteMovie(movie._id)} className="btn-delete">
-              Delete 🗑
-            </button>
-          </div>
-        )
+        return <MovieCard key={movie._id} movie={movie} clickToDelete={deleteMovie}/>
       })}
     </div>
   )
